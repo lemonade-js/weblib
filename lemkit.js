39965dff8e6736b2c9fb28cf7b46f8d1b58bcffe
gooberlemonade <104174925+lemonade-js@users.noreply.github.com>
2025-06-17 18:46:21 -0600

    fixed arrayShuffle

--- lemkit.js
+++ lemkit.js
@@ -24,6 +24,8 @@ window.lemkit = {
             const j = Math.floor(Math.random() * (i + 1));
             [array[i], array[j]] = [array[j], array[i]];
         }
+
+        return array;
     },
 
     getChance(...chances) {
